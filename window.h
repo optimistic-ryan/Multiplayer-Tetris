307985eb87459da88a12c12a315be184071c1044
Ryan <106937234+optimistic-ryan@users.noreply.github.com>
2023-09-16 13:58:51 -0400

    Update window.h

--- window.h
+++ window.h
@@ -13,20 +13,14 @@ class Xwindow {
   unsigned long colours[10];
 
  public:
-  Xwindow(int width=500, int height=500);  // Constructor; displays the window.
-  ~Xwindow();                              // Destructor; destroys the window.
+  Xwindow(int width=500, int height=500);  
+  ~Xwindow();                              
   Xwindow(const Xwindow&) = delete;
   Xwindow &operator=(const Xwindow&) = delete;
 
-  // Available colours.
   enum {White=0, Black, Red, Green, Blue, Cyan, Yellow, Magenta, Orange, Brown};
-
-  // Draws a rectangle
   void fillRectangle(int x, int y, int width, int height, int colour=Black);
-
-  // Draws a string
   void drawString(int x, int y, std::string msg);
-
 };
 
 #endif
