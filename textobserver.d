bcc7e749b3a6519fe665c21e0e6bd7c250dfc4ba
Ryan <106937234+optimistic-ryan@users.noreply.github.com>
2023-10-29 11:53:25 -0400

    Delete textobserver.d

--- textobserver.d
+++
@@ -1,4 +0,0 @@
-textobserver.o: textobserver.cc textobserver.h observer.h game.h \
- commands.h trienode.h board.h Block.h Levels/level.h Levels/levelZero.h \
- Levels/level.h Levels/levelOne.h Levels/levelTwo.h Levels/levelThree.h \
- Levels/levelFour.h Levels/levelFivePlus.h subject.h
