f71d59640f3275e889e49f8822e0c57a99c7120a
Ryan <106937234+optimistic-ryan@users.noreply.github.com>
2023-10-29 11:51:29 -0400

    Delete commands.d

--- commands.d
+++
@@ -1,4 +0,0 @@
-commands.o: commands.cc commands.h trienode.h game.h board.h Block.h \
- Levels/level.h Levels/levelZero.h Levels/level.h Levels/levelOne.h \
- Levels/levelTwo.h Levels/levelThree.h Levels/levelFour.h \
- Levels/levelFivePlus.h subject.h
